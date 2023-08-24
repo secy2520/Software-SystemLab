84c35999032a518f5e4326b5f2a78c6bfaad482b
secy2520 <sadana.jass.2520@gmail.com>
2023-08-24 21:21:53 +0530

    10th program

---
+++ 10.c
@@ -0,0 +1,34 @@
+#include<sys/types.h>
+#include<unistd.h>
+#include<sys/stat.h>
+#include<stdio.h>
+#include<fcntl.h>
+int main()
+{
+int fd_write;
+char buff[10];
+ fd_write=open("trust",O_RDONLY | O_WRONLY);
+if(fd_write ==-1)
+{
+printf("Koi faayda nhi h, do whatever you want");
+}
+printf("Daaliye file mei janaab jo daalna hai\n");
+scanf("%s",buff);
+int err_write=write(fd_write, buff, sizeof(buff));
+if(err_write==-1)
+{
+printf("Kuch toh gadbad hai janaab\n");
+}
+char buff1[10];
+int offset=lseek(fd_write,20,SEEK_SET);
+printf("Abhi hmaara pointer iss position par hai %d", offset);
+printf("Daaliye file mei janaab jo ab 10 bytes k baad daalna hai");
+scanf("%s",buff1);
+int err1_write=write(fd_write, buff1, sizeof(buff1));
+if(err1_write==-1)
+{
+printf("Gadbad hai rehene dijiye");
+}
+close(fd_write); 
+return 0;
+}
