a7ba570b2a96d89fc309ebddbad1541bac3cacc7
secy2520 <sadana.jass.2520@gmail.com>
2023-08-24 21:26:08 +0530

    12th program

---
+++ 12.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<fcntl.h>
+#include<unistd.h>
+int main()
+{
+int fd1;
+fd1=open("destfile", O_RDONLY);
+if(fd1 == -1)
+{
+printf("Error opening file");
+}
+int flag=fcntl(fd1, F_GETFL);  //F_GETFL is used to retrieve  assosicated flags with the file
+if(flag==-1)
+{
+printf("Bakwaas");
+}
+int acc_mode=flag & O_ACCMODE; //O_ACCMODE is there in fcntl.h
+if(acc_mode== O_RDONLY)
+{
+printf("The file is opened in Read Mode Only");
+}
+if(acc_mode== O_WRONLY)
+{
+printf("The file is opened in Read Mode Only");
+}
+if(acc_mode== O_RDWR)
+{
+printf("The file is opened in Read Mode Only");
+}
+close(fd1);
+return 0;
+}
