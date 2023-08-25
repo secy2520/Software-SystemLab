fb8464aeab564c0cb05d8b660a85fa9cbbd401a8
secy2520 <sadana.jass.2520@gmail.com>
2023-08-25 22:56:57 +0530

    16th program b

---
+++ 16b.c
@@ -0,0 +1,26 @@
+#include <stdio.h>      /*************  Read Lock Setter  *************/
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include<stdlib.h>
+int main()
+{
+  struct flock lock, trylock;
+  int fd;
+
+  fd = open("record1.txt", O_RDONLY);
+  lock.l_type = F_RDLCK;
+  lock.l_start = 0;
+  lock.l_whence = SEEK_SET;
+  lock.l_len = 50;
+  trylock = lock;
+  fcntl(fd, F_GETLK, &lock);
+  if (lock.l_type == F_WRLCK)
+  {
+      printf("File is write-locked by process.\n");
+      exit(1);
+  }
+  fcntl(fd, F_SETLK, &trylock);
+  pause();
+}
