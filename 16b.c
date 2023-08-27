d3fc711aa1375ec7031eb653d13ec40113c33f82
secy2520 <sadana.jass.2520@gmail.com>
2023-08-27 18:28:58 +0530

    modified:   16b.c 	modified:   18a.c 	modified:   18b.c 	modified:   8.c

--- 16b.c
+++ 16b.c
@@ -6,21 +6,35 @@
 #include<stdlib.h>
 int main()
 {
-  struct flock lock, trylock;
+  struct flock lock, savelock;
   int fd;
 
   fd = open("record1.txt", O_RDONLY);
   lock.l_type = F_RDLCK;
   lock.l_start = 0;
   lock.l_whence = SEEK_SET;
-  lock.l_len = 50;
-  trylock = lock;
+  lock.l_len = 0;
+  savelock = lock;
   fcntl(fd, F_GETLK, &lock);
   if (lock.l_type == F_WRLCK)
   {
-      printf("File is write-locked by process.\n");
-      exit(1);
+      printf("File is write-locked by process....Waiting!!\n");
+      while(lock.l_type==F_WRLCK)
+  {
+    fcntl(fd, F_GETLK, &lock);
   }
-  fcntl(fd, F_SETLK, &trylock);
-  pause();
+  }
+if(  fcntl(fd,F_SETLKW, &savelock)==-1)
+{
+printf("Error getting the read lock...");
+}
+  printf("Now getting the read lock....\n");
+ printf("Press enter to come out of the critical section...\n");
+//getchar();
+getchar();
+savelock.l_type=F_UNLCK;
+  fcntl(fd, F_SETLK, &savelock);
+  printf("You are successfully out of the critical section and unlocked.\n");
+  close(fd);
+return 0;
 }
