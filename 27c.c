e712ed9babf8ef5858ce5cef0f841a88fc8cc5b3
secy2520 <sadana.jass.2520@gmail.com>
2023-09-04 21:59:51 +0530

    27th c part

---
+++ 27c.c
@@ -0,0 +1,13 @@
+#include<unistd.h>
+#include<stdio.h>
+#include<stdlib.h>
+int main(void)
+{
+if(execlp("/bin/ls", "ls", "-R","-l", NULL)==-1)
+{
+printf("There is some error...\n");
+}
+return 0;
+}
+
+
