b9d48dce2c5318f8ccb4866a0f78f718f8504a62
secy2520 <sadana.jass.2520@gmail.com>
2023-09-04 22:00:45 +0530

    29th program

---
+++ 29.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<sched.h>
+#include<stdlib.h>
+
+int main(int argc, char *argv[])
+{
+if(argc!=2)
+{
+printf("You are requested to enter the process name also.. \n");
+}
+pid_t pid1=atoi(argv[1]);
+switch(sched_getscheduler(pid1))
+{
+case SCHED_FIFO:printf("Scheduling Policy is FIFO..\n");break;
+case SCHED_RR:printf("Schedulung Policy is RR...\n");break;
+case SCHED_OTHER :printf("Some other Scheduling Policy..\n");break;
+default:printf("Unknow Policy...\n");break;
+}
+struct sched_param param;
+param.sched_priority=99;
+if(sched_setscheduler(pid1, SCHED_FIFO,&param)==-1)
+{
+printf("There is some error modifying the scheduling policy...\n");
+exit(1);
+}
+printf("Scheduling Policy has been modified to FIFO..\n");
+return 0;
+}
