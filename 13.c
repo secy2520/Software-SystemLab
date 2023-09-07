deee823660a54505101ae05421cb9013294ec3d3
secy2520 <82306041+secy2520@users.noreply.github.com>
2023-09-07 15:44:26 +0530

    13th program

--- 13.c
+++ 13.c
@@ -2,7 +2,7 @@
 -------------------------------------------------------
 -------------------------------------------------------
 
-Name-12.c
+Name-13.c
 Author-Jass Sadana
 Description-Write a program to wait for a STDIN for 10 seconds using select. Write a proper print statement to verify whether the data is available within 10 seconds or not (check in $man 2 select).
 
