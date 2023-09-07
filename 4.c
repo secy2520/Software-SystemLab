3b1e6406670c9632924200ca07efa16aae8f4610
secy2520 <82306041+secy2520@users.noreply.github.com>
2023-09-07 15:14:06 +0530

    4th program

--- 4.c
+++ 4.c
@@ -1,3 +1,16 @@
+/*
+-------------------------------------------------------
+-------------------------------------------------------
+
+Name-4.c
+Author-Jass Sadana
+Description-Write a program to open an existing file with read write mode. Try O_EXCL flag also.
+
+-------------------------------------------------------
+-------------------------------------------------------
+*/
+
+
 #include<stdio.h>
 #include<fcntl.h>
 #include<stdio.h>
@@ -11,7 +24,7 @@ const char *data="Now Im writing through CLI";
 int fd=open(filename,O_WRONLY | O_EXCL, 0666);
 write(fd,data,strlen(data));
 if(fd==-1)
-perror("There is some erro");
+perror("There is some error");
 close(fd);
 return 0;
 }
