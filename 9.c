0436efb6ad64d2e8ce0fa0737afcec2a1bee84b5
secy2520 <82306041+secy2520@users.noreply.github.com>
2023-09-07 15:23:13 +0530

    9th program

--- 9.c
+++ 9.c
@@ -1,3 +1,24 @@
+/*
+-------------------------------------------------------
+-------------------------------------------------------
+
+Name-9.c
+Author-Jass Sadana
+Description-Write a program to print the following information about a given file.
+a. inode
+b. number of hard links
+c. uid
+d. gid
+e. size
+f. block size
+g. number of blocks
+h. time of last access
+i. time of last modification
+j. time of last change
+
+-------------------------------------------------------
+-------------------------------------------------------
+*/
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <stdint.h>
