174e638390e57c368277e54c860ebdd7b668e4c7
secy2520 <82306041+secy2520@users.noreply.github.com>
2023-09-07 15:36:23 +0530

    10th program

--- 10.c
+++ 10.c
@@ -2,7 +2,7 @@
 -------------------------------------------------------
 -------------------------------------------------------
 
-Name-9.c
+Name-10.c
 Author-Jass Sadana
 Description-Write a program to open a file with read write mode, write 10 bytes, move the file pointer by 10 bytes (use lseek) and write again 10 bytes.
 a. check the return value of lseek
