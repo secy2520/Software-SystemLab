ed09c6c55204de7e0ef8eb0abf31188f5e3740a8
secy2520 <82306041+secy2520@users.noreply.github.com>
2023-09-07 15:08:56 +0530

    2nd program

--- 2.c
+++ 2.c
@@ -2,7 +2,7 @@
 -------------------------------------------------------
 -------------------------------------------------------
 
-Name-1a.c
+Name-2.c
 Author-Jass Sadana
 Description-Write a simple program to execute in an infinite loop at the background.
 
