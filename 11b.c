c557cbe30a644b3aeab7c82edc13d733b07fe628
secy2520 <82306041+secy2520@users.noreply.github.com>
2023-09-07 15:38:37 +0530

    11b program

--- 11b.c
+++ 11b.c
@@ -1,3 +1,17 @@
+/*
+-------------------------------------------------------
+-------------------------------------------------------
+
+Name-11b.c
+Author-Jass Sadana
+Description-Write a program to open a file, duplicate the file descriptor and append the file with both the descriptors and check whether the file is updated properly or not.
+b) use dup2
+
+-------------------------------------------------------
+-------------------------------------------------------
+*/
+
+
 #include <unistd.h>
 #include<stdio.h>
 #include<fcntl.h>
