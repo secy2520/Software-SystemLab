d35135902626bb12030b814413ec4112a2c4a19b
secy2520 <82306041+secy2520@users.noreply.github.com>
2023-09-07 15:44:04 +0530

    14th program

--- 14.c
+++ 14.c
@@ -1,3 +1,18 @@
+/*
+-------------------------------------------------------
+-------------------------------------------------------
+
+Name-14.c
+Author-Jass Sadana
+Description-Write a program to find the type of a file.
+a. Input should be taken from command line.
+b. program should be able to identify any type of a file.
+-------------------------------------------------------
+-------------------------------------------------------
+*/
+
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
