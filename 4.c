14de48f3e6e766d14bdc5d21bab86fccaf8e9ea1
secy2520 <82306041+secy2520@users.noreply.github.com>
2023-09-07 15:17:25 +0530

    Update 4.c

--- 4.c
+++ 4.c
@@ -21,7 +21,7 @@ int main()
 {
 const char *filename="sample.txt";
 const char *data="Now Im writing through CLI";
-int fd=open(filename,O_WRONLY | O_EXCL, 0666);
+int fd=open(filename,O_RDWR | O_EXCL, 0666);
 write(fd,data,strlen(data));
 if(fd==-1)
 perror("There is some error");
