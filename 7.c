65113aa60435c58e9e58fec74c9f9d7ed5a65aa2
secy2520 <82306041+secy2520@users.noreply.github.com>
2023-09-08 14:27:15 +0530

    7th program

--- 7.c
+++ 7.c
@@ -5,7 +5,7 @@
 Name-7.c
 Author-Jass Sadana
 Description-Write a program to copy file1 into file2 ($cp file1 file2).
-
+Date-18-8-23
 -------------------------------------------------------
 -------------------------------------------------------
 */
