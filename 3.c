5b3f05ff92a47d2f078957b080ea7d7e2da96bba
secy2520 <82306041+secy2520@users.noreply.github.com>
2023-09-08 14:24:39 +0530

    3rd program

--- 3.c
+++ 3.c
@@ -5,7 +5,7 @@
 Name-3.c
 Author-Jass Sadana
 Description-Write a program to create a file and print the file descriptor value. Use creat ( ) system call
-
+Date-11-8-23
 -------------------------------------------------------
 -------------------------------------------------------
 */
