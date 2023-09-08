8f900f4a4f9b2e09602eea2e4f572333fbf5d27a
secy2520 <82306041+secy2520@users.noreply.github.com>
2023-09-08 14:34:42 +0530

    17th b program

--- 17b.c
+++ 17b.c
@@ -5,7 +5,8 @@
 Name-17b.c
 Author-Jass Sadana
 Description- Write a program to simulate online ticket reservation. Implement write lock
-Write a program to open a file, store a ticket number and exit. Write a separate program, to open the file, implement write lock, read the ticket number, increment the number and print the new ticket number then close the file.
+Write a program to open a file, store a ticket number and exit. 
+Date-25-8-23
 -------------------------------------------------------
 -------------------------------------------------------
 */
