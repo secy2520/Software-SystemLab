a053377a4e73ab6174d03f7742da47db4db36a82
secy2520 <82306041+secy2520@users.noreply.github.com>
2023-09-08 14:29:39 +0530

    11th c program

--- 11f.c
+++ 11f.c
@@ -6,7 +6,7 @@ Name-11c.c
 Author-Jass Sadana
 Description-Write a program to open a file, duplicate the file descriptor and append the file with both the descriptors and check whether the file is updated properly or not.
 c) use fcntl
-
+Date-18-8-23
 -------------------------------------------------------
 -------------------------------------------------------
 */
