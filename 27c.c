c57cda7fbb13d1c9a6a3638e48d5ee158f9e8c9a
secy2520 <82306041+secy2520@users.noreply.github.com>
2023-09-08 14:17:10 +0530

    27th c program

--- 27c.c
+++ 27c.c
@@ -1,3 +1,16 @@
+/*
+-------------------------------------------------------
+-------------------------------------------------------
+Name - 27c.c
+Author - Jass Sadana
+Description - Write a program to execute ls -Rl by the following system calls
+c. execlp
+Date-1-8-23
+-------------------------------------------------------
+-------------------------------------------------------
+*/
+
+
 #include<unistd.h>
 #include<stdio.h>
 #include<stdlib.h>
