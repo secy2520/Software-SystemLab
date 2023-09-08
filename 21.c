d246c941794c83dae568418da6de1ce54d792098
secy2520 <82306041+secy2520@users.noreply.github.com>
2023-09-08 13:53:27 +0530

    21st program

--- 21.c
+++ 21.c
@@ -1,3 +1,14 @@
+/*
+-------------------------------------------------------
+-------------------------------------------------------
+
+Name-21.c
+Author-Jass Sadana
+Description-Write a program, call fork and print the parent and child process id.
+-------------------------------------------------------
+-------------------------------------------------------
+*/
+
 #include<stdio.h>
 #include<unistd.h>
 int main(void)
@@ -8,6 +19,8 @@ printf("Process ID of the child is.... %d\n", getpid());
 }
 else
 {
+int status;
+wait(&status);
 printf("Process ID of the parent is.... %d \n", getppid());
 }
 return 0;
