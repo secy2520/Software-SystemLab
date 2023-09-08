90824476c34084c75ce87d3203a7ac5f82e0d0d8
secy2520 <82306041+secy2520@users.noreply.github.com>
2023-09-08 14:28:10 +0530

    10th program

--- 10.c
+++ 10.c
@@ -7,7 +7,7 @@ Author-Jass Sadana
 Description-Write a program to open a file with read write mode, write 10 bytes, move the file pointer by 10 bytes (use lseek) and write again 10 bytes.
 a. check the return value of lseek
 b. open the file with od and check the empty spaces in between the data.
-
+Date-18-8-23
 -------------------------------------------------------
 -------------------------------------------------------
 */
